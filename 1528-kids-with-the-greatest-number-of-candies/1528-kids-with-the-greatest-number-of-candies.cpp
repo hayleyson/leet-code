fe805d0ce355a46d738a0992c75ef9afd6f62fee
Hayley Son <hayleyson92@gmail.com>
2023-08-24 10:57:02 +0900

    Time: 0 ms (100%), Space: 9 MB (32.44%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -3,10 +3,8 @@ public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         
         vector<bool> result;
-        int maxCandy = *std::max_element(candies.begin(), candies.end());
-        int threshold = maxCandy - extraCandies;
-        for (int i = 0; i < candies.size(); i++){
-            if (candies[i] >= threshold)
+        for (int n: candies){
+            if (n >= (*std::max_element(candies.begin(), candies.end()) - extraCandies))
                 result.push_back(true);
             else
                 result.push_back(false);
