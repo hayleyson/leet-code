4cf4d378b73fcdc57e9cca63dad66ce6065e9a78
Hayley Son <hayleyson92@gmail.com>
2023-08-25 20:04:58 +0900

    Time: 6 ms (53.72%), Space: 7.2 MB (88.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,21 +4,48 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        std::stack<string> wordstack;
-        string word;
-        string outstring="";
-        stringstream ss(s);
-        while (ss >> word){
-            wordstack.push(word);
+
+        /* answer taken from leetcode */
+        // Reverse string 
+        reverse(s.begin(), s.end());
+        //elpmaxe   doog a
+        // Reverse every word
+        int i = 0, j = 0;
+        while(i != s.size()){
+            while(j < s.size() && s[j] != ' ') j++;
+            reverse(s.begin() + i, s.begin() + j);
+            i = j;
+            while(i < s.size() && s[i] == ' ') i++, j++;
+        } //example   good a
+        // Remove extra ' '. If extra space, bring next non-space character to the front.
+        i = 0, j = 0;
+        while(j < s.size()){
+            bool new_word = false;
+            while(j < s.size() && s[j] == ' '){
+                new_word = true;
+                j++;
+            }
+            if(j == s.size()) break;
+            if(new_word && i > 0) s[i++] = ' ';
+            s[i++] = s[j++];
         }
+        // example   good a
+        // example g good a
+        // example gogood a
+        // example gooood a
+        // example goodod a
+        // example good a a
+        s = s.substr(0, i);
+        return s;
 
-        outstring=outstring+wordstack.top();
-        wordstack.pop();
-        while(wordstack.empty()==false){
-
-            outstring=outstring+" "+wordstack.top();
-            wordstack.pop();
+        /* my initial solution using sstream 
+        string word, outstring="";
+        stringstream ss(s);
+        ss >> outstring;
+        while (ss >> word){
+            outstring= word + " " + outstring;
         }
         return outstring;
+        */
     }
 };
