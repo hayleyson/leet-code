32ae58461a8472bb4e4476c6ca36429d7f7cd331
Hayley Son <hayleyson92@gmail.com>
2023-08-25 16:26:46 +0900

    Time: 0 ms (100%), Space: 8.4 MB (11.61%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-
-    bool isVowel(char ch){
-        ch = tolower(ch);
-        if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')) return true;
-        else return false;
-    }
-
     string reverseVowels(string s) {
         vector<int> indices;
         for (int i=0; i<s.length();i++){
             char ch=s[i];
-            if(isVowel(ch)){
+            if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')||(ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')){
                 indices.push_back(i);
             }
         }
         for(int i=0;i<indices.size()/2;i++){
             char tmp=s[indices[i]];
+            // s[indices[i]]=s[indices[indices.size()-i-1]];
+            // s[indices[indices.size()-i-1]]=tmp;
             swap(s[indices[i]], s[indices[indices.size()-i-1]]);
         }
         return s;
