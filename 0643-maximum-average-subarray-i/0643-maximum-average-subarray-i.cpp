77962e7317f04f5336542f2ed4470ae6966059af
Hayley Son <hayleyson92@gmail.com>
2023-08-31 22:38:47 +0900

    Time: 137 ms (72.03%), Space: 109.6 MB (39.33%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,12 +1,44 @@
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) {
+//         int tmpSum = std::accumulate(nums.begin(), nums.begin()+k,0);
+//         int maxSum = tmpSum;
+//         for (int i = 0; i < nums.size()-k; i++){
+//             tmpSum = tmpSum - nums[i] + nums[i+k];
+//             maxSum = max(maxSum, tmpSum);
+//         }
+//         return (double)maxSum/k;
+//     }
+// };
+
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int tmpSum = std::accumulate(nums.begin(), nums.begin()+k,0);
-        int maxSum = tmpSum;
-        for (int i = 0; i < nums.size()-k; i++){
-            tmpSum = tmpSum - nums[i] + nums[i+k];
-            maxSum = max(maxSum, tmpSum);
+        if (nums.size() == 1) {
+            return static_cast<double>(nums[0]);
         }
-        return (double)maxSum/k;
+        
+        int start = 0;
+        int end = k;
+        double average = 0.0;
+        
+        // Calculate the average of the first window
+        for (int i = 0; i < k; i++) {
+            average += static_cast<double>(nums[i]) / k;
+        }
+        
+        // Initialize the maximum average
+        double max_average = average;
+        
+        // Slide the window and update the maximum average
+        while (end < nums.size()) {
+            average = average - static_cast<double>(nums[start]) / k;
+            average = average + static_cast<double>(nums[end]) / k;
+            max_average = max(max_average, average);
+            start++;
+            end++;
+        }
+        
+        return max_average;
     }
 };
