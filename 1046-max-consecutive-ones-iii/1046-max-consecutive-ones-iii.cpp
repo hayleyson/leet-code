1c1e2bce8cc5e58337121fc9f0f9fd1615b85083
Hayley Son <hayleyson92@gmail.com>
2023-09-02 01:20:19 +0900

    Time: 49 ms (65.45%), Space: 55.5 MB (21.04%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        int l=0;
+        int r=0;
+        for (r=0; r<nums.size();r++){
+            if (nums[r]==0) {k--;}
+            if (k<0){
+                if(nums[l]==0) {k++;}
+                l++;
+            }
+        }
+        return r-l;
+    }
+};
