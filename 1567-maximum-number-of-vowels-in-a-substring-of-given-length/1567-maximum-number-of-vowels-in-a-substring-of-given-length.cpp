5f24f8983f828fa01ee9ad3e0e6d1be6037dbe3f
Hayley Son <hayleyson92@gmail.com>
2023-09-02 00:58:48 +0900

    Time: 24 ms (41.49%), Space: 9.9 MB (59.71%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+
+    bool checkVowel(char ch){
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') return true;
+        else return false;
+    }
     int maxVowels(string s, int k) {
         int count = 0;
         for (int i=0; i<k; i++){
-            if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') count+=1;
+            if (checkVowel(s[i])) count+=1;
         }
         int max_count = count;
         if (s.size()==k || max_count==k) return max_count;
 
         for (int i=k; i<s.size();i++){
-            if (s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u') count--;
-            if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') count++;
+            if (checkVowel(s[i-k])) count--;
+            if (checkVowel(s[i])) count++;
             max_count = max_count > count? max_count: count;
             if (max_count==k) return max_count;
         }
