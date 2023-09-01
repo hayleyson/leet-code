a1331235daf3d4d8e8d989f9ca8b8e4e7c00763a
Hayley Son <hayleyson92@gmail.com>
2023-09-02 00:37:45 +0900

    Time: 125 ms (82.66%), Space: 18.4 MB (5.05%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,14 +1,27 @@
 class Solution:
-    def isVowel(self, ch: str):
-        return 1 if ch in 'aeiou' else 0
 
     def maxVowels(self, s: str, k: int) -> int:
-        count = 0
-        for i in range(k):
-            count += self.isVowel(s[i])
-        max_count = count
-        if (k < len(s)) and (max_count<k):
-            for i in range(k, len(s)):
-                count = count - self.isVowel(s[i-k]) + self.isVowel(s[i])
-                max_count = max(max_count, count)
-        return max_count
+        isVowels = [1 if ch in 'aeiou' else 0 for ch in s]
+        maxCount = count = sum(isVowels[:k])
+        if (maxCount == k) or (len(s) == k):
+            return maxCount
+        for i in range(k, len(s)):
+            count = count + isVowels[i] - isVowels[i-k]
+            maxCount = max(maxCount, count)
+            if maxCount == k:
+                return maxCount
+        return maxCount
+
+    # def isVowel(self, ch: str):
+    #     return 1 if ch in 'aeiou' else 0
+
+    # def maxVowels(self, s: str, k: int) -> int:
+    #     count = 0
+    #     for i in range(k):
+    #         count += self.isVowel(s[i])
+    #     max_count = count
+    #     if (k < len(s)):
+    #         for i in range(k, len(s)):
+    #             count = count - self.isVowel(s[i-k]) + self.isVowel(s[i])
+    #             max_count = max(max_count, count)
+    #     return max_count
