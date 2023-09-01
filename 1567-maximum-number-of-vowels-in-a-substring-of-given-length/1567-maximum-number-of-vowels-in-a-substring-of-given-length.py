9947c608c16c6e68cea50455d6eec57bef4c6a31
Hayley Son <hayleyson92@gmail.com>
2023-09-02 00:50:30 +0900

    Time: 172 ms (45.24%), Space: 17.1 MB (69.71%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -8,11 +8,14 @@ class Solution:
         for i in range(k):
             count += self.isVowel(s[i])
         max_count = count
-        if (k < len(s)):
-            for i in range(k, len(s)):
-                if self.isVowel(s[i-k]):
-                    count -=1
-                if self.isVowel(s[i]):
-                    count += 1 
-                max_count = max(max_count, count)
+        if (len(s)==k) or (max_count==k):
+            return max_count
+        for i in range(k, len(s)):
+            if self.isVowel(s[i-k]):
+                count -=1
+            if self.isVowel(s[i]):
+                count += 1 
+            max_count = max(max_count, count)
+            if (max_count==k):
+                return max_count
         return max_count
