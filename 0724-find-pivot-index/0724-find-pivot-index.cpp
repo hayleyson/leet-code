dcb5cbe971f9a8a4e6ba3282e76a62d6e173fc18
Hayley Son <hayleyson92@gmail.com>
2023-09-02 14:34:31 +0900

    Time: 20 ms (61.78%), Space: 31.1 MB (84.75%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int left = 0;
-        int right = std::accumulate(nums.begin()+1, nums.end(), 0);
+        int right = 0;
+        for (int i=1; i < nums.size(); i++){
+            right += nums[i];
+        }
         if (left==right) return 0;
         for(int i=1; i < nums.size(); i++){
-            left+=nums[i-1];
-            right-=nums[i];
+            left += nums[i-1];
+            right -= nums[i];
             if (left==right) return i;
         }
         return -1;
