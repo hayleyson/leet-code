224724f0f9f2f810de77e62adf5dd8ac11ae75cb
Hayley Son <hayleyson92@gmail.com>
2023-09-02 22:13:48 +0900

    Time: 474 ms (15.49%), Space: 19.1 MB (56.07%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -5,31 +5,38 @@ class Solution(object):
         :rtype: int
         """
 
-        # move i & increase count by 1 until hitting 0
-        # when hitting 0, 
-        # if delete_index is not set (first time encountering 0)
-        #    count = i - delete_index - 1
-        #    delete_index = i
-        # else
-        #    count = count
-        #    delete_index = i
-        # maxCount = max(count, maxCount)
+        # maxCount = count = 0
+        # delete_index = -1
+        # for i in range(len(nums)):
+        #     if nums[i] == 1: # pass 1's
+        #         count += 1
+        #     else:   # if encounter 0
+        #         if delete_index == -1: # if it's first time encountering
+        #             delete_index = i
+        #         else: # if it already had encountered 0
+        #             count = i - delete_index - 1
+        #             delete_index = i
+        #     if count > maxCount:
+        #         maxCount = count
+        # if delete_index == -1: # if all elems are 1's. Remove one 1 -> decrease maxCount by 1.
+        #     maxCount -= 1
 
-        maxCount = count = 0
-        delete_index = -1
-        for i in range(len(nums)):
-            if nums[i] == 1: # pass 1's
-                count += 1
-            else:   # if encounter 0
-                if delete_index == -1: # if it's first time encountering
-                    delete_index = i
-                else: # if it already had encountered 0
-                    count = i - delete_index - 1
-                    delete_index = i
-            if count > maxCount:
-                maxCount = count
-        if delete_index == -1: # if all elems are 1's. Remove one 1 -> decrease maxCount by 1.
-            maxCount -= 1
+        # return maxCount
 
-        return maxCount
+        ## solution by shivam-727
+        i, j = 0, 0
+        mx = 0
+        c0 = 0
+        n = len(nums)
+
+        while(j < len(nums)):
+            if nums[j] == 0:
+                c0 += 1
+            while (i < len(nums) and c0 == 2):
+                if nums[i] == 0:
+                    c0 -= 1
+                i+= 1
+            mx = max(mx, j - i)
+            j += 1
+        return mx
         
