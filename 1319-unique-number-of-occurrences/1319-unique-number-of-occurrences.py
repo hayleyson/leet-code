0f5290f008a782ec1d8137e4482dd3b6176825d1
Hayley Son <hayleyson92@gmail.com>
2023-09-02 17:34:29 +0900

    Time: 47 ms (55.86%), Space: 16.4 MB (81.18%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        countDic={}
+        for i in arr:
+            if i in countDic:
+                countDic[i]+=1
+            else:
+                countDic[i]=1
+        return len(countDic.values())==len(set(countDic.values()))
