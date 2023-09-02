8a223b26aaf404420f217e1a179b7853cdb38482
Hayley Son <hayleyson92@gmail.com>
2023-09-02 17:46:45 +0900

    Time: 0 ms (100%), Space: 8.4 MB (22.32%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    // bool uniqueOccurrences(vector<int>& arr) {
-    //     std::unordered_map<int, int> intCount;
-    //     for(const int& i: arr){
-    //         intCount[i]++;
-    //     }
-    //     std::unordered_map<int, int> countCount;
-    //     for (auto elem: intCount){
-    //         if(countCount.count(elem.second)!=0) return false;
-    //         countCount[elem.second]++;
-    //     }
-    //     return true;
-    // }
-
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> mp;
-        set<int> s;
-        for(auto p : arr){
-            mp[p]++;
+        std::unordered_map<int, int> intCount;
+        for(const int& i: arr){
+            intCount[i]++;
         }
-        for(auto pr : mp){
-            s.insert(pr.second);
+        std::unordered_map<int, int> countCount;
+        for (auto elem: intCount){
+            if(countCount.count(elem.second)!=0) return false;
+            countCount[elem.second]++;
         }
-        return mp.size()==s.size();
+        return true;
+
+        /* c.f. solution by adipycoder */
+        // unordered_map<int,int> mp;
+        // set<int> s;
+        // for(auto p : arr){
+        //     mp[p]++;
+        // }
+        // for(auto pr : mp){
+        //     s.insert(pr.second);
+        // }
+        // return mp.size()==s.size();
     }
 };
