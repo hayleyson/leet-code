d1e6994abb76580249a543f700ec2535d6d3d636
Hayley Son <hayleyson92@gmail.com>
2023-09-02 21:34:49 +0900

    Time: 359 ms (46.13%), Space: 17.7 MB (21.78%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -7,26 +7,23 @@ class Solution(object):
 
         # move i & increase count by 1 until hitting 0
         # after hitting 0, 
-        # if last_delete_index is not None
-        #    count = i - last_delete_index
+        # if delete_index is not None
+        #    count = i - delete_index
         #    last_delete_index = i
 
         maxCount = count = 0
-        last_delete_index = -1
+        delete_index = -1
         for i in range(len(nums)):
             if nums[i] == 1:
                 count += 1
-                if count > maxCount:
-                    maxCount = count
+            elif delete_index == -1:
+                delete_index = i
             else:
-                if last_delete_index == -1:
-                    last_delete_index = i
-                else:
-                    count = i - last_delete_index - 1
-                    last_delete_index = i
-                    if count > maxCount:
-                        maxCount = count
-        if last_delete_index == -1:
+                count = i - delete_index - 1
+                delete_index = i
+            if count > maxCount:
+                    maxCount = count
+        if delete_index == -1:
             maxCount -= 1
 
         return maxCount
