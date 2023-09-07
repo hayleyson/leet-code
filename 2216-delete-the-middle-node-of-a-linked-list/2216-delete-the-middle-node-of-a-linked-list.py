74fe7bca749e906e9ff24fb753a6d92392b7ba7c
Hayley Son <hayleyson92@gmail.com>
2023-09-07 20:55:38 +0900

    Time: 1295 ms (58.21%), Space: 62.9 MB (84.45%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -20,18 +20,19 @@ class Solution:
             return None # return sentinel
         else:
             delId = int(size/2) # int() does the rounding-down
-            prevNode = None
             curNode = head # initialize curNode as head
             curId = 0 # initialize curIndex as 0
-            while curId != delId: # if (while) curId hasn't reached the index to delete
+            while curId < (delId-1): # if (while) curId hasn't reached the one index previous to delId
                 prevNode = curNode
                 curNode = curNode.next # jump to the next node
                 curId += 1 # increase curId
-            # now curId == delId and curNode is the curId'th node from the head
+            # now curId == delId-1 and curNode is the ()curId-1)'th node from the head
             # how to remove a node from linked list?
             # set the previous node's next to current node's next 
             # unset current node's next
-            prevNode.next = curNode.next
-            curNode.next = None
+            # prevNode.next = curNode.next
+            delNode = curNode.next
+            curNode.next = delNode.next
+            delNode.next = None
             return head
             
