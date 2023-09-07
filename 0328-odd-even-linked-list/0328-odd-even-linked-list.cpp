2ec909f030742a0abb9aeaa2934511fe42843a34
Hayley Son <hayleyson92@gmail.com>
2023-09-07 22:20:28 +0900

    Time: 7 ms (85.02%), Space: 10.5 MB (24.48%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,14 +11,14 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr) return head;
+        if (head == nullptr || head -> next == nullptr || head->next->next==nullptr) return head;
         
         ListNode* curNode = head;
         ListNode* tmpNode, *prevNode;
         ListNode* evenStart=head-> next;
         int i = 0;
         // connecting node to the next next node
-        while(curNode->next != nullptr){ // loop over until curNode is not the last elem of linked list
+        while(curNode->next != nullptr){ // loop ends when curNode is the last elem of linked list
             tmpNode=curNode -> next;
             curNode->next = tmpNode-> next; // connect current node to a node 2 steps ahead
             prevNode = curNode; // save current curNode for later
@@ -29,6 +29,24 @@ public:
         if (i%2==0) curNode -> next = evenStart;  // if the linked list length is odd
         else prevNode->next = evenStart; // if the linked list length is even
         return head;
+
+
+        // /* c.f. solution by _LeetCoder25_ */
+        // ListNode* oddEvenList(ListNode* head) {
+        // if(!head || !head->next || !head->next->next) return head;
+        
+        // ListNode *odd = head;
+        // ListNode *even = head->next;
+        // ListNode *even_start = head->next;
+        
+        // while(odd->next && even->next){
+        //     odd->next = even->next; //Connect all odds
+        //     even->next = odd->next->next;  //Connect all evens
+        //     odd = odd->next;
+        //     even = even->next;
+        // }
+        // odd->next = even_start;   //Place the first even node after the last odd node.
+        // return head; 
         
     }
 };
