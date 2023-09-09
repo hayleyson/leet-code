16042a3ba84f4f6067d475316dc2facc3e29fe9b
Hayley Son <hayleyson92@gmail.com>
2023-09-10 02:05:15 +0900

    Time: 176 ms (62.7%), Space: 13.8 MB (80.71%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,33 @@
+class Solution(object):
+
+    def __DFSHelp(self, node, neighbors, visited):
+        if visited[node] == False:
+            visited[node] = True
+            for u in neighbors[node]:
+                self.__DFSHelp(u, neighbors, visited)
+
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        V = range(len(isConnected))
+        neighbors={}
+        visited={}
+
+        for v in V:
+            neighbors[v]=[]
+            visited[v]=False
+
+        for v in V:
+            for i, u in enumerate(isConnected[v]):
+                if u == 1:
+                    neighbors[v].append(i)
+        
+        count = 0
+        for v in V:
+            if not visited[v]:
+                count+=1
+                self.__DFSHelp(v, neighbors, visited)
+
+        return count
