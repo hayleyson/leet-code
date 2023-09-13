6cb3dbae57e97c3394800831ac873274fa82ddfa
Hayley Son <hayleyson92@gmail.com>
2023-09-13 22:41:15 +0900

    Time: 56 ms (68.36%), Space: 16.4 MB (59.36%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -26,23 +26,16 @@ class Solution:
             minute += 1
 
             for x, y in rottens:
-                print((x, y))
                 for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
                     x_adj = x+dx
                     y_adj = y+dy
-                    print("adj: ", x_adj, y_adj)
                     if 0<= x_adj and x_adj < nRows and 0 <=y_adj and y_adj < nCols:
-                        print("adj value: ", grid[x_adj][y_adj])
                         if grid[x_adj][y_adj]==1:
                             new_rottens.append((x_adj, y_adj))
-                            print("new_rottens: ", new_rottens)
                             nfresh -= 1
                             grid[x_adj][y_adj] = 2
                             if nfresh == 0:
                                 return minute
-                    else:
-                        print("index out of range")
-                        continue 
             rottens = new_rottens
         if nfresh == 0:
             return minute
