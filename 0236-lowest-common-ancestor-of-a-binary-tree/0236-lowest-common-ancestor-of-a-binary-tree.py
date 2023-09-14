ef1e0284ec36b73f074ce82515459415ad9f5cdc
Hayley Son <hayleyson92@gmail.com>
2023-09-15 00:38:58 +0900

    Time: 83 ms (20.46%), Space: 29 MB (15.53%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+
+    def __DFTHelp(self, curNode, p, q):
+        if curNode is None:
+            return False, None
+        else:
+            
+            resSelf = (curNode.val in [p.val, q.val])
+            resL, retNodeL = self.__DFTHelp(curNode.left, p, q)
+            resR, retNodeR = self.__DFTHelp(curNode.right, p, q)
+
+            if retNodeL: # if LCA is already found.
+                return True, retNodeL
+            elif retNodeR:
+                return True, retNodeR
+            else:
+                trueCount = sum([resSelf, resL, resR])
+                if trueCount == 2: # if two of the above conditions are true.
+                    print("!!!")
+                    return True, curNode
+                elif trueCount == 1: # if only one is met.
+                    return True, None
+                else: # if none is met.
+                    return False, None
+            
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.__DFTHelp(root, p, q)[1]
