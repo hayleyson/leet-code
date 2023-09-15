cfcdae98240522cfc3b5e2a87eab77df3e6d965f
Hayley Son <hayleyson92@gmail.com>
2023-09-16 01:17:31 +0900

    Time: 53 ms (82.25%), Space: 17.6 MB (86.08%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,7 +7,10 @@
 
 class Solution:
 
-    """solution by wonderlives 1. brute force"""
+    """solution by wonderlives 1. brute force
+    memory O(1), time O(N^2) or O(NlogN)
+    # ---
+    
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.result = 0 #  instead of using global scope, declared result as an attribute of Solution class -> shared across multiple function calls
         self.dfs(root,targetSum) 
@@ -30,3 +33,27 @@ class Solution:
         # check if path sums starting from this node excluding this node's value equals to target-node.val
         self.test(node.left, target-node.val)
         self.test(node.right, target-node.val)
+    """
+
+    """solution by wonderlives 2. memorization
+    memory O(N), time O(N)
+    """
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        self.result = 0 
+        cache={0:1}
+        self.dfs(root,targetSum,0,cache) 
+        return self.result
+
+    def dfs(self, node, target, currPathSum, cache):
+        if node is None: # base case
+            return
+        
+        currPathSum += node.val
+        oldPathSum = currPathSum - target
+        self.result += cache.get(oldPathSum, 0)
+        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+
+        self.dfs(node.left, target, currPathSum, cache)
+        self.dfs(node.right, target, currPathSum, cache)
+
+        cache[currPathSum] -= 1
