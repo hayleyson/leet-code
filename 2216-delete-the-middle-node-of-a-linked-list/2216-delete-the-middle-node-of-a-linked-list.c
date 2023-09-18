8af3e86b23e45a33a08d480b8d21936f72bcc96e
Hayley Son <hayleyson92@gmail.com>
2023-09-18 15:16:13 +0900

    Time: 370 ms (26.17%), Space: 78.7 MB (5.72%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.c
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.c
@@ -22,24 +22,22 @@ struct ListNode* deleteMiddle(struct ListNode* head){
     int midpoint = getSizeLL(head) / 2;
     if (midpoint==0){
         return NULL;
-    }
-    
-    int index=0;
-    ListNode* curr=head;
-    ListNode* prev=NULL;
-        
-    while (curr != NULL){
-        // printf("val: %d, index: %d\n", curr->val, index);
-        if (index == midpoint){
-            // printf("Deleting this node. (index: %d, val: %d)\n", index, curr->val);
-            prev->next = curr->next;
-            free(curr);
-            break;
+    } else {
+        int index=0;
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+            
+        while (curr != NULL){
+            if (index == midpoint){
+                prev->next = curr->next;
+                free(curr);
+                break;
+            }
+            prev = curr;
+            curr = curr->next;
+            index++;
         }
-        prev = curr;
-        curr = curr->next;
-        index++;
+        return head;
+        
     }
-    return head;
-    
 }
